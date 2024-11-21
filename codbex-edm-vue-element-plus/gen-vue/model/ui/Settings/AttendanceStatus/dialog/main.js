5c44e65a0b962808ca6bd918f82abb7c93f868ab
ThuF <jordan.p.pavlov@gmail.com>
2024-11-21 21:11:18 +0200

    Add UI for AttendanceStatus

---
+++ codbex-edm-vue-element-plus/gen-vue/model/ui/Settings/AttendanceStatus/dialog/main.js
@@ -0,0 +1,118 @@
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import { Plus } from '@element-plus/icons-vue'
+// import { createI18n } from 'vue-i18n'
+
+// import en from './locales/en.json' with { type: "json" };
+// import bg from './locales/bg.json' with { type: "json" };
+
+// const i18n = createI18n({
+//     locale: 'en',
+//     fallbackLocale: 'en',
+//     messages: {
+//         en: en,
+//         bg: bg,
+//     }
+// });
+
+const app = createApp({
+    setup() {
+        return {
+            isCreate: false,
+            isUpdate: false,
+            isPreview: false,
+            Plus: Plus,
+        }
+    },
+    created() {
+        this.$messageHub.subscribe(this.onOpenDialog, 'app.Settings.AttendanceStatus.openDialog');
+        this.$messageHub.subscribe(this.onConfirmDialog, 'app.Settings.AttendanceStatus.openDialog.confirm');
+    },
+    data: function () {
+        return {
+            entity: {}
+        };
+    },
+    methods: {
+        onOpenDialog: function (event) {
+            if (event.data.isCreate) {
+                this.isCreate = true;
+                this.isUpdate = false;
+                this.isPreview = false;
+                this.entity = {};
+            } else if (event.data.isUpdate) {
+                this.isCreate = false;
+                this.isUpdate = true;
+                this.isPreview = false;
+                this.entity = event.data.entity;
+            } else {
+                this.isCreate = false;
+                this.isUpdate = false;
+                this.isPreview = true;
+                this.entity = event.data.entity;
+            }
+        },
+        onConfirmDialog: async function (event) {
+            if (this.isCreate) {
+                try {
+                    const response = await fetch('/services/ts/codbex-edm-vue-element-plus/gen/model/api/Settings/AttendanceStatusService.ts', {
+                        method: 'POST',
+                        body: JSON.stringify(this.entity)
+                    });
+                    if (response.status === 201) {
+                        this.$messageHub.post({}, 'app.Settings.AttendanceStatus.refreshData');
+                        this.$messageHub.post({
+                            message: `Contract Type successfully created.`,
+                            type: 'success',
+                        }, 'app.showMessage');
+                    } else {
+                        const error = await response.json();
+                        this.showErrorMessage('Failed to create Contract Type', `Error message: ${error.message}`);
+                    }
+                } catch (e) {
+                    const message = `Error message: ${e.message}`;
+                    console.error(message, e);
+                    this.showErrorMessage('Failed to create Contract Type', message);
+                }
+            } else if (this.isUpdate) {
+                try {
+                    const response = await fetch(`/services/ts/codbex-edm-vue-element-plus/gen/model/api/Settings/AttendanceStatusService.ts/${this.entity.Id}`, {
+                        method: 'PUT',
+                        body: JSON.stringify(this.entity)
+                    });
+                    if (response.status === 200) {
+                        this.$messageHub.post({}, 'app.Settings.AttendanceStatus.refreshData');
+                        this.$messageHub.post({
+                            message: `Contract Type successfully updated.`,
+                            type: 'success',
+                        }, 'app.showMessage');
+                    } else {
+                        const error = await response.json();
+                        this.showErrorMessage('Failed to edit Contract Type', `Error message: ${error.message}`);
+                    }
+                } catch (e) {
+                    const message = `Error message: ${e.message}`;
+                    console.error(message, e);
+                    this.showErrorMessage('Failed to edit Contract Type', message);
+                }
+            }
+            this.$messageHub.post({}, 'app.closeDialog');
+        },
+        saveCreate: async function () {
+        },
+        showErrorMessage: function (title, message) {
+            this.$messageHub.post({
+                title: title,
+                message: message,
+                type: 'error',
+                duration: 0,
+            }, 'app.showNotification');
+        },
+    }
+});
+
+app.config.globalProperties.$messageHub = new FramesMessageHub();
+
+// app.use(i18n);
+app.use(ElementPlus);
+app.mount('#app');
---
+++ codbex-edm-vue-element-plus/gen-vue/model/ui/Settings/AttendanceStatus/main.js
@@ -0,0 +1,177 @@
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import { Plus } from '@element-plus/icons-vue'
+import { createI18n } from 'vue-i18n'
+
+import en from './locales/en.json' with { type: "json" };
+import bg from './locales/bg.json' with { type: "json" };
+
+const i18n = createI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: {
+        en: en,
+        bg: bg,
+    }
+});
+
+const app = createApp({
+    setup() {
+        return {
+            tourSearchInput: null,
+            tourEditButton: null,
+            tourDeleteButton: null,
+            Plus: Plus,
+        }
+    },
+    created() {
+        this.$messageHub.subscribe(this.start, 'app.startTour');
+        this.$messageHub.subscribe(this.changeLocale, 'app.changeLocale');
+        this.$messageHub.subscribe(this.confirmDelete, 'app.Settings.AttendanceStatus.confirmDelete');
+        this.$messageHub.subscribe(this.refreshData, 'app.Settings.AttendanceStatus.refreshData');
+    },
+    data: function () {
+        return {
+            currentPage: 1,
+            pageSize: 10,
+            total: 0,
+            startTour: false,
+            loading: true,
+            tableData: [],
+            dialogDetailVisible: false,
+            dialogEditVisible: false,
+            dialogCreateVisible: false,
+            selectedEntity: null,
+            selectedEntityIndex: null,
+        };
+    },
+    methods: {
+        start: function () {
+            this.startTour = true;
+        },
+        showDetail: function (_index, entity) {
+            debugger
+            this.$messageHub.post({
+                title: 'Contract Type Details',
+                path: '/services/web/codbex-edm-vue-element-plus/gen-vue/model/ui/Settings/AttendanceStatus/dialog/index.html',
+                dialogTopic: 'app.Settings.AttendanceStatus.openDialog',
+                dialogData: {
+                    isPreview: true,
+                    entity: JSON.parse(JSON.stringify(entity))
+                },
+            }, 'app.openDialog');
+        },
+        handleEdit: function (index, entity) {
+            this.$messageHub.post({
+                title: 'Update Contract Type',
+                path: '/services/web/codbex-edm-vue-element-plus/gen-vue/model/ui/Settings/AttendanceStatus/dialog/index.html',
+                dialogTopic: 'app.Settings.AttendanceStatus.openDialog',
+                dialogData: {
+                    isUpdate: true,
+                    entity: JSON.parse(JSON.stringify(entity))
+                },
+            }, 'app.openDialog');
+        },
+        handleCreate: function () {
+            this.$messageHub.post({
+                title: 'Create Contract Type',
+                path: '/services/web/codbex-edm-vue-element-plus/gen-vue/model/ui/Settings/AttendanceStatus/dialog/index.html',
+                dialogTopic: 'app.Settings.AttendanceStatus.openDialog',
+                dialogData: {
+                    isCreate: true,
+                },
+            }, 'app.openDialog');
+        },
+        showErrorMessage: function (title, message) {
+            this.$messageHub.post({
+                title: title,
+                message: message,
+                type: 'error',
+                duration: 0,
+            }, 'app.showNotification');
+        },
+        handleDelete: async function (index, entity) {
+            this.selectedIndex = index;
+            this.selectedEntity = entity;
+            const event = {
+                title: 'Delete Contract Type?',
+                description: 'Are you sure you want to delete Contract Type? This action cannot be undone.',
+                options: {
+                    confirmButtonText: 'Yes',
+                    cancelButtonText: 'No',
+                    type: 'warning',
+                },
+                confirmTopic: 'app.Settings.AttendanceStatus.confirmDelete',
+            }
+            this.$messageHub.post(event, 'app.showConfirm');
+        },
+        confirmDelete: async function (event) {
+            if (event.isConfirmed) {
+                try {
+                    const response = await fetch(`/services/ts/codbex-edm-vue-element-plus/gen/model/api/Settings/AttendanceStatusService.ts/${this.selectedEntity.Id}`, { method: 'DELETE' })
+                    if (response.status === 204) {
+                        this.tableData.splice(this.selectedIndex, 1);
+                        this.$messageHub.post({
+                            message: `Contract Type was deleted successfully.`,
+                            type: 'success',
+                        }, 'app.showMessage');
+                    } else {
+                        const error = await response.json();
+                        this.showErrorMessage('Failed to delete Contract Type', `Error message: ${error.message}`);
+                    }
+                } catch (e) {
+                    const message = `Error message: ${e.message}`;
+                    console.error(message, e);
+                    this.showErrorMessage('Failed to delete Contract Type', message);
+                }
+            }
+            this.selectedIndex = undefined;
+            this.selectedEntity = undefined;
+        },
+        changeLocale: function (event) {
+            i18n.global.locale = event.data;
+        },
+        refreshData: async function () {
+            this.loading = true;
+
+            const responseCount = await fetch('/services/ts/codbex-edm-vue-element-plus/gen/model/api/Settings/AttendanceStatusService.ts/count');
+            if (!responseCount.ok) {
+                const error = await responseCount.json();
+                this.showErrorMessage('Failed to get Contract Type count', `Error message: ${error.message}`);
+                throw new Error(`Response status: ${responseCount.status}`);
+            }
+
+            this.total = parseInt(await responseCount.text());
+
+            const offset = (this.currentPage - 1) * this.pageSize;
+            const limit = this.pageSize;
+
+            const response = await fetch(`/services/ts/codbex-edm-vue-element-plus/gen/model/api/Settings/AttendanceStatusService.ts?$offset=${offset}&$limit=${limit}`);
+            if (!response.ok) {
+                const error = await response.json();
+                this.showErrorMessage('Failed to load Contract Type', `Error message: ${error.message}`);
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            setTimeout(async function (context) {
+                context.loading = false
+                context.tableData = await response.json();
+            }, 1000, this);
+        },
+        handlePageChange: async function (currentPage, pageSize) {
+            this.currentPage = currentPage;
+            this.pageSize = pageSize;
+
+            await this.refreshData();
+        }
+    },
+    mounted: async function () {
+        await this.refreshData();
+    }
+});
+
+app.config.globalProperties.$messageHub = new FramesMessageHub();
+
+app.use(i18n);
+app.use(ElementPlus);
+app.mount('#app');
